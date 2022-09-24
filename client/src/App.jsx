09f80b4c992e3d1cc705e20bce797e4cf5105c46
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-24 16:22:33 -0700

    More initial filestructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <>
+      <Navbar />
+        <Home />
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className="navbar-brand">
+
+        </div>
+        <div className="nav-link-container">
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="#">Login</a>
+        </div>
+    </nav>
+  );
+}
+
