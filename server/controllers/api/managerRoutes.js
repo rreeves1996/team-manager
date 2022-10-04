c50e8459862a66aaf9ec17084531297762d5a9f6
Eyesack <isaac.laflamme@gmail.com>
2022-10-04 10:19:54 -0700

    CRUD operations for all models are actually operational now that i remmebered to save team and manager routes

--- server/controllers/api/managerRoutes.js
+++ server/controllers/api/managerRoutes.js
@@ -54,4 +54,50 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update a Manager
+//* PUT /api/managers/:manId
+router.put('/:manId', async (req, res) => {
+  try {
+    const man = await Manager.update(req.body, {
+      where: {
+        id: req.params.manId
+      }
+    });
+
+    if (!man) {
+      res.status(404).json({ errMessage: 'No Manager found with given ID'});
+      return;
+    };
+    
+    const updatedman = await Manager.findByPk(req.params.manId);
+
+    res.status(200).json(updatedman);
+  } catch (err) {
+    res.status(500).json(err)
+  };
+});
+
+// Delete a Manager
+//* DELETE /api/managers/:manId
+router.delete('/:manId', async (req, res) => {
+  try {
+    const deletedman = await Manager.findByPk(req.params.manId);
+
+    const manToDelete = await Manager.destroy({
+      where: {
+        id: req.params.manId
+      }
+    });
+
+    if (!manToDelete) {
+      res.status(404).json({ errMessage: 'No Manager found with given ID'});
+      return;
+    };
+    
+    res.status(200).json({message: 'Manager succesfully deleted', Manager: deletedman});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
