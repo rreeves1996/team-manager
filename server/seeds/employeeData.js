63bf79f256acde746c1cc237999606e6cc38f483
Eyesack <isaac.laflamme@gmail.com>
2022-10-10 22:50:29 -0700

    seeds now work except for employee seeds for an unknown reason

--- server/seeds/employeeData.js
+++ server/seeds/employeeData.js
@@ -2,35 +2,193 @@ const { Employee } = require('../models');
 
 const employeeData = [
   //* Team 1 Employees
+
   {
-    name: '',
-    role_id: 0,
-    team_id: 0,
-    manager_id: 0,
+    name: "Lynden O'Reilly",
+    role_id: 1,
+    team_id: 1,
+    manager_id: 1,
+    // id: 1
   },
   {
-    name: '',
-    role_id: 0,
-    team_id: 0,
-    manager_id: 0,
+    name: 'Nuha Hogan',
+    role_id: 2,
+    team_id: 1,
+    manager_id: 2,
+    // id: 2
   },
   {
-    name: '',
-    role_id: 0,
-    team_id: 0,
-    manager_id: 0,
+    name: 'Abdur Muir',
+    role_id: 2,
+    team_id: 1,
+    manager_id: 2,
+    // id: 3
   },
   {
-    name: '',
-    role_id: 0,
-    team_id: 0,
-    manager_id: 0,
+    name: 'Aryaan Stewart',
+    role_id: 1,
+    team_id: 1,
+    manager_id: 1,
+    // id: 4
   },
   {
-    name: '',
-    role_id: 0,
-    team_id: 0,
+    name: 'Sara Pierce',
+    role_id: 3,
+    team_id: 1,
+    manager_id: 1,
+    // id: 5
+  },
+
+  //* Team 2 Employees
+
+  {
+    name: 'Harvey-Lee David',
+    role_id: 6,
+    team_id: 2,
+    manager_id: 3,
+    // id: 6
+  },
+  {
+    name: 'Glenda Turner',
+    role_id: 5,
+    team_id: 2,
+    manager_id: 3,
+    // id: 7
+  },
+  {
+    name: 'Samiyah Owens',
+    role_id: 5,
+    team_id: 2,
+    manager_id: 3,
+    // id: 8
+  },
+  {
+    name: 'Farrah Cabrera',
+    role_id: 4,
+    team_id: 2,
+    manager_id: 4,
+    // id: 9
+  },
+  {
+    name: 'Elysia Rodgers',
+    role_id: 4,
+    team_id: 2,
+    manager_id: 4,
+    // id: 10
+  },
+
+  //* Team 3 Employees
+
+  {
+    name: 'Zaki Murray',
+    role_id: 7,
+    team_id: 3,
+    manager_id: 5,
+    // id: 11
+  },
+  {
+    name: 'Javan Bloom',
+    role_id: 9,
+    team_id: 3,
+    manager_id: 6,
+    // id: 12
+  },
+  {
+    name: 'Kieron Greaves',
+    role_id: 8,
+    team_id: 3,
+    manager_id: 5,
+    // id: 13
+  },
+  {
+    name: 'Benn Dickson',
+    role_id: 7,
+    team_id: 3,
+    manager_id: 5,
+    // id: 14
+  },
+  {
+    name: 'Gideon Garrett',
+    role_id: 9,
+    team_id: 3,
+    manager_id: 6,
+    // id: 15
+  },
+
+  //* Team 4 Employees
+
+  {
+    name: 'Rhiannan Mccormack',
+    role_id: 12,
+    team_id: 4,
+    manager_id: 7,
+    // id: 16
+  },
+  {
+    name: 'Simeon Mcintosh',
+    role_id: 12,
+    team_id: 4,
+    manager_id: 7,
+    // id: 17
+  },
+  {
+    name: 'Andrea Smyth',
+    role_id: 11,
+    team_id: 4,
+    manager_id: 8,
+    // id: 18
+  },
+  {
+    name: 'Lorena Galindo',
+    role_id: 11,
+    team_id: 4,
+    manager_id: 8,
+    // id: 19
+  },
+  {
+    name: 'Ivan Mccaffrey',
+    role_id: 10,
+    team_id: 4,
+    manager_id: 7,
+    // id: 20
+  },
+
+  //* Team 5 Employees
+
+  {
+    name: 'Maizie Bannister',
+    role_id: 14,
+    team_id: 5,
+    manager_id: 9,
+    // id: 21
+  },
+  {
+    name: 'Nayla Kaiser',
+    role_id: 13,
+    team_id: 5,
     manager_id: 0,
+    // id: 22
+  },
+  {
+    name: 'Eleri Lang',
+    role_id: 14,
+    team_id: 5,
+    manager_id: 10,
+    // id: 23
+  },
+  {
+    name: 'Olaf Vinson',
+    role_id: 15,
+    team_id: 5,
+    manager_id: 9,
+    // id: 24
+  },
+  {
+    name: 'Borys Kaufman',
+    role_id: 13,
+    team_id: 5,
+    manager_id: 10,
+    // id: 25
   },
 ];
 
--- server/seeds/roleData.js
+++ server/seeds/roleData.js
@@ -2,34 +2,108 @@ const { Role } = require('../models');
 
 const roleData = [
   //* Team 1 Roles
-  {
-    title: '',
-    salary: 0,
-    team_id: 0,
-  },
 
   {
-    title: '',
-    salary: 0,
-    team_id: 0,
+    title: 'Senior Executive Coordinator',
+    salary: 175000,
+    team_id: 1,
+    // id: 1
+  },
+  {
+    title: 'Party Planning Commitee',
+    salary: 85000,
+    team_id: 1,
+    // id: 2
+  },
+  {
+    title: 'Vagrant',
+    salary: 60000,
+    team_id: 1,
+    // id: 3
   },
 
-  {
-    title: '',
-    salary: 0,
-    team_id: 0,
-  },
+  //* Team 2 Roles
 
   {
-    title: '',
-    salary: 0,
-    team_id: 0,
+    title: 'Leader of The Free World',
+    salary: 100000000,
+    team_id: 2,
+    // id: 4
+  },
+  {
+    title: 'Professional Liason',
+    salary: 79000,
+    team_id: 2,
+    // id: 5
+  },
+  {
+    title: 'Not A Doctor',
+    salary: 94000,
+    team_id: 2,
+    // id: 6
   },
 
+  //* Team 3 Roles
+
   {
-    title: '',
-    salary: 0,
-    team_id: 0,
+    title: 'Dude Who Hangs Out',
+    salary: 80000,
+    team_id: 3,
+    // id: 7
+  },
+  {
+    title: 'OnlyFans Coordinator',
+    salary: 65000,
+    team_id: 3,
+    // id: 8
+  },
+  {
+    title: "Guy Who Still Says 'YOLO'",
+    salary: 118000,
+    team_id: 3,
+    // id: 9
+  },
+
+  //* Team 4 Roles
+
+  {
+    title: 'Legally Employed',
+    salary: 115000,
+    team_id: 4,
+    // id: 10
+  },
+  {
+    title: 'Illegally Employed',
+    salary: 115000,
+    team_id: 4,
+    // id: 11
+  },
+  {
+    title: 'Not Employed At All',
+    salary: 200000,
+    team_id: 4,
+    // id: 12
+  },
+
+  //* Team 5 Roles
+
+  {
+    title: 'Groundhog Wrangler',
+    salary: 90000,
+    team_id: 5,
+    // id: 13
+  },
+  {
+    title: 'Moon-Truther',
+    salary: 108000,
+    team_id: 5,
+    // id: 14
+  },
+  {
+    title: 'Not Even a Real Guy',
+    salary: 76000,
+    team_id: 5,
+    // id: 15
   },
 ];
 
