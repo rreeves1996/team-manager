75916cf1c427716a61d78a0406829c41092e9af2
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-10 21:03:58 -0700

    Form submit started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,47 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppProvider } from './utils/GlobalState';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import Footer from './components/Footer';
 import About from './components/About';
+
 
 import './assets/style/style.css';
 import InitPrompt from './components/InitPrompt';
 import { render } from '@testing-library/react';
 
+
 export default function App() {
   const [page, changePage] = useState(() => {
-    if (localStorage.getItem('teamName')) {
-      return 'Home';
+    if(localStorage.getItem("teamName")) {
+      return "Home";
     } else {
-      return 'Init';
+      return "Init";
     }
   });
 
+
   const renderPage = () => {
-    switch (page) {
-      case 'Init':
-        return <InitPrompt page={page} handlePageChange={handlePageChange} />;
-      case 'Home':
+    switch(page) {
+      case "Init":
+          return <InitPrompt page={page} handlePageChange={handlePageChange} />;
+      case "Home":
         return <Home handlePageChange={handlePageChange} />;
       default:
-        return <Home handlePageChange={handlePageChange} />;
+        return <Home handlePageChange={handlePageChange} />
     }
-  };
+  }
 
   const handlePageChange = (page) => changePage(page);
 
   return (
-    <Router>
-      <AppProvider>
-        <Navbar />
-        <main>{renderPage()}</main>
-        <Footer />
-      </AppProvider>
-    </Router>
+    <>
+      <Navbar />
+      <main>
+        {renderPage()}
+      </main>
+      <Footer />
+    </>
   );
 }
 
