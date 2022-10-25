4dcd89cd6d27d16661160ebb49460406205a70bc
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-24 20:25:32 -0700

    Temporary more stats fix, weird REST route fix
    REST routes were getting back html from requests instead of JSON, changed app.use(routes) to be executed earlier in server.js and the issue fixed itself

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const session = require('express-session');
-const routes = require('./controllers');
 const cors = require("cors");
-
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
@@ -27,9 +23,10 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -39,8 +36,8 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-  
-app.use(routes);
+
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
