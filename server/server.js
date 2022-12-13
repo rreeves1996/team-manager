251667309e4ad7bdf3da75985f4921206cce4121
Ryan Reeves <reeves0ryan@gmail.com>
2022-12-12 21:33:05 -0800

    Nevermind

--- server/server.js
+++ server/server.js
@@ -29,10 +29,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 sequelize.sync({ force: false }).then(() => {
